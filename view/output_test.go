f52b5217453b894e8d3557cf85e30e7cd1394fef
tenfy <zhongtenghui@gf.com.cn>
2018-12-22 14:37:51 +0800

    test output.go

--- view/output_test.go
+++ view/output_test.go
@@ -4,57 +4,52 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/tenfyzhong/orion/model"
 )
 
 func TestSidebarTitle(t *testing.T) {
-	if sidebarTitle() != "Num Method Host Status" {
-		t.FailNow()
-	}
+	assert.Equal(t, "Num Method Host Status", sidebarTitle())
 }
 
-func TestSidebarStringMNil(t *testing.T) {
-	if messageSidebarString(nil) != "" {
-		t.FailNow()
-	}
+func messageSidebarNil(t *testing.T) {
+	assert.Equal(t, "", messageSidebarString(nil))
+	assert.Equal(t, "", messageSidebarString(&model.Message{
+		Num: 1,
+	}))
 }
 
-func TestSidebarStringReqNil(t *testing.T) {
-	m := &model.Message{}
-	if messageSidebarString(m) != "" {
-		t.FailNow()
-	}
-}
-
-func TestSidebarStringRspNil(t *testing.T) {
-	req, err := http.NewRequest("GET", "https://www.tenfy.cn", nil)
-	if err != nil || req == nil {
-		t.Fatal("err should be nil, req should not be nil", err, req)
-	}
+func TestMessageSidebarString(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://tenfy.cn", nil)
+	assert.NoError(t, err)
 	m := &model.Message{
 		Num: 1,
 		Req: req,
+		Rsp: nil,
 	}
-	expect := "  1    GET www.tenfy.cn Pending"
-	actual := messageSidebarString(m)
-	if actual != expect {
-		t.Fatalf("expect: %s, but get: %s", expect, actual)
-	}
+	assert.Equal(t, "  1    GET tenfy.cn Pending", messageSidebarString(m))
 }
 
-func TestSidebarString(t *testing.T) {
-	req, err := http.NewRequest("GET", "https://www.tenfy.cn", nil)
-	if err != nil || req == nil {
-		t.Fatal("err should be nil, req should not be nil", err, req)
-	}
+func TestMessageSidebarStringWithRsp(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://tenfy.cn", nil)
+	assert.NoError(t, err)
 	m := &model.Message{
 		Num: 1,
 		Req: req,
 		Rsp: &http.Response{},
 	}
-	expect := "  1    GET www.tenfy.cn OK"
-	actual := messageSidebarString(m)
-	if actual != expect {
-		t.Fatalf("expect: %s, but get: %s", expect, actual)
-	}
+	assert.Equal(t, "  1    GET tenfy.cn OK", messageSidebarString(m))
+}
+
+func TestSidebarStringGetMessageNumEmpty(t *testing.T) {
+	assert.Equal(t, 0, sidebarStringGetMessageNum(""))
+	assert.Equal(t, 0, sidebarStringGetMessageNum(" "))
+	assert.Equal(t, 0, sidebarStringGetMessageNum("G "))
+	assert.Equal(t, 0, sidebarStringGetMessageNum(" G"))
+}
+
+func TestSidebarStringGetMessageNum(t *testing.T) {
+	assert.Equal(t, 1, sidebarStringGetMessageNum("  1 GET"))
+	assert.Equal(t, 1, sidebarStringGetMessageNum("1 GET"))
+	assert.Equal(t, 1, sidebarStringGetMessageNum(" 1 1"))
 }
