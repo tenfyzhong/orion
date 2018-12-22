56053be5dce9d93b642ce3dcf4db107e65a04a4b
tenfy <zhongtenghui@gf.com.cn>
2018-12-22 17:27:03 +0800

    add an echobody tool

---
+++ tools/echobody/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	body, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Println(string(body))
+	w.Write(body)
+}
